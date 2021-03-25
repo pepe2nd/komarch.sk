bd8ceecae63f88ecfe4ee69bcf7535f6de9f2abd
Andrej Bilec <andrej.bilec@gmail.com>
2021-03-25 19:20:18 +0100

    Tailwind setup - design colors set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /*
     Default configuration:
     https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+
  */
 module.exports = {
     purge: [
@@ -15,6 +16,17 @@ module.exports = {
             lg: '1024px',
             xl: '1280px',
         },
+        colors: {
+            white: '#FFFFFF',
+            black: '#000000',
+            blue: '#001DFF',
+            gray: {
+                100: '#F5F5F5',
+                300: '#969696',
+                500: '#707070',
+                900: '#040405',
+            }
+        }
     },
     variants: {},
     plugins: [],
