e80d55e006924e245fe027746dc85f5fbba85404
Andrej Bilec <andrej.bilec@gmail.com>
2021-03-25 18:35:38 +0100

    Tailwind CSS setup added

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,9 @@ require('laravel-mix-polyfill');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/styleguide.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/styleguide.scss', 'public/css');
+    .postCss("resources/css/app.css", "public/css", [
+        require("tailwindcss"),
+    ]);
 
 if (mix.inProduction()) {
     mix.version()
