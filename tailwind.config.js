44b54d4a461d810e01c281f839b5837741bfe2b3
Andrej Bilec <andrej.bilec@gmail.com>
2021-03-25 19:08:26 +0100

    Tailwind setup - screen breakpoints added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
         './resources/**/*.js',
         './resources/**/*.vue',
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+        },
     },
-    variants: {
-        extend: {},
-    },
+    variants: {},
     plugins: [],
 }
