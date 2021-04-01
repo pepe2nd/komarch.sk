f178c6ab71a9761ce8d1a468940f62114418f7bf
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-01 13:41:25 +0200

    Redundant filters removed

--- resources/js/components/filters.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#filters :input").change(function(e) {
-        var form = $(this).closest("form");
-        form.find(":input").filter(function(){ return !this.value; }).attr("disabled", "disabled");
-        form.submit();
-    });
-});
