b7b76614d72a45e6e44d3753ffbc5c3fcfc4b018
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-09 12:13:32 +0200

    Tile fontsizes fixed for mobile

--- resources/views/components/tile.blade.php
+++ resources/views/components/tile.blade.php
@@ -1,5 +1,5 @@
 <div class="group relative border-t h-full pt-12 pb-16 md:pt-8 md:pb-12 hover:bg-blue hover:border-blue transition duration-100">
-    <h3 class="text-2xl leading-snug px-5 group-hover:text-white transition duration-100">
+    <h3 class="text-xl text-center md:text-2xl md:text-left leading-snug px-5 group-hover:text-white transition duration-100">
         <a href="{{ $url }}" class="block link-area">
             {{ $title }} →
         </a>
