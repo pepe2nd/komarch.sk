0f20ab03f3b68e32760fb1d72063799bbee6494f
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-19 17:26:13 +0200

    Homepage tile spacing added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
 <div class="container mx-auto px-4">
     <x-intro text="Slovenská Komora Architektov je odbornou organizáciou, ktorá sa zameriava na vzdelávanie a informovanie verejnosti o potrebách kvalitnej architektúry"/>
 
-    <div class="mt-24 md:grid grid-cols-2 gap-10">
+    <div class="mt-32 md:grid grid-cols-2 gap-10">
         <div class="flex" style="height: 300px">
             <span class="m-auto">Clock here</span>
         </div>
