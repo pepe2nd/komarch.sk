7ee0a2a0f4c14fb3eb1ad2e1d70b4b7ee5f53d35
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-05-19 15:34:20 +0200

    include types, roles and topics in documents api

--- app/Http/Resources/DocumentResource.php
+++ app/Http/Resources/DocumentResource.php
@@ -14,6 +14,15 @@ class DocumentResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'download_count' => $this->download_count,
+            'created_at' => trim($this->created_at->formatLocalized('%e. %B %Y')),
+            'updated_at' => trim($this->updated_at->formatLocalized('%e. %B %Y')),
+            'types' => TagResource::collection($this->whenLoaded('types')),
+            'topics' => TagResource::collection($this->whenLoaded('topics')),
+            'roles' => TagResource::collection($this->whenLoaded('roles')),
+        ];
     }
 }
