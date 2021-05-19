b75fdf9a549e8cb5b1f7b9080e56381019e184bd
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-05-19 15:26:58 +0200

    add API route + resource for documents

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Http\Resources\PostResource;
 
+use App\Models\Document;
+use App\Http\Resources\DocumentResource;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,7 +26,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/posts', function (Request $request) {
     $posts = Post::published()->orderBy('published_at', 'desc');
-    $per_page = min($request->get('per_page', 6), 15);
+    $per_page = (int)min($request->get('per_page', 6), 15);
     if ($request->has('categories')) {
         $posts->withAnyTags($request->input('categories', []));
     }
@@ -36,4 +39,14 @@ Route::get('/posts', function (Request $request) {
 Route::get('/related-posts', function (Request $request) {
     $related = Post::take(10)->inRandomOrder()->get();
     return PostResource::collection($related);
+});
+
+
+Route::get('/documents', function (Request $request) {
+    $documents = Document::orderBy('created_at', 'desc');
+    $per_page = (int)min($request->get('per_page', 10), 15);
+    if ($request->has('filters')) {
+        $documents->withAnyTags($request->input('filters', []));
+    }
+    return DocumentResource::collection($documents->paginate($per_page));
 });
