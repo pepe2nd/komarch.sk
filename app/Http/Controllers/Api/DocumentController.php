626e0b07c1dca7f91c0c82d6fa25933fb68478aa
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-05-26 01:51:25 +0200

    add sortby, direction and quick search to documents api

--- app/Http/Controllers/Api/DocumentController.php
+++ app/Http/Controllers/Api/DocumentController.php
@@ -12,8 +12,43 @@ class DocumentController extends Controller
 {
     public function index(Request $request)
     {
-        $documents = Document::orderBy('created_at', 'desc');
+        $documents = $this->loadDocuments($request);
+
+        // search
+        if ($request->filled('q')) {
+            $documents->where('name', 'like', '%' .$request->input('q') . '%');
+        }
+
+        // sort
+        $documents->orderBy(
+            $request->input('sortby', 'created_at'),
+            $request->input('direction', 'desc')
+        );
+
         $per_page = (int)min($request->get('per_page', 10), 15);
+        return DocumentResource::collection($documents->paginate($per_page));
+    }
+
+    public function filters(Request $request)
+    {
+        $documents = $this->loadDocuments($request)->get();
+        $filters = collect();
+        foreach (Document::$filterable as $filter) {
+            $filters[$filter] = $documents->pluck($filter)->flatten()->countBy('name');
+        }
+        return $filters;
+    }
+
+    public function download($id, Request $request)
+    {
+        return Document::find($id)->increment('download_count');
+    }
+
+    private function loadDocuments(Request $request)
+    {
+        $documents = Document::query();
+
+        // apply filters
         if ($request->has('types')) {
             $documents->withAnyTags($request->input('types', []), 'document-type');
         }
@@ -23,21 +58,7 @@ class DocumentController extends Controller
         if ($request->has('roles')) {
             $documents->withAnyTags($request->input('roles', []), 'document-role');
         }
-        return DocumentResource::collection($documents->paginate($per_page));
-    }
 
-    public function filters(Request $request)
-    {
-        $filters = collect();
-        $documents = Document::get();
-        foreach (Document::$filterable as $filter) {
-            $filters[$filter] = $documents->pluck($filter)->flatten()->countBy('name');
-        }
-        return $filters;
-    }
-
-    public function download($id, Request $request)
-    {
-        return (bool) Document::find($id)->increment('download_count');
+        return $documents;
     }
 }
