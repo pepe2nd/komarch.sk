ef405ac882e05ed7b0f6fd0ac330bab5a188cb94
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-02 12:15:35 +0200

    Filter group component added

--- resources/js/components/DocumentsOverviewFilters.vue
+++ resources/js/components/DocumentsOverviewFilters.vue
@@ -1,58 +1,40 @@
 <template>
   <div class="md:flex items-start">
-    <div class="mr-12 mb-8">
-      <h3 class="mb-5">
-        Typ dokumentu:
-      </h3>
-      <input-checkbox
-        v-for="option in filters.types"
-        :key="option.key"
-        :value="value"
-        :option="option"
-        class="mr-12 py-1"
-        @input="onInput"
-      />
-    </div>
-    <div class="mr-12 mb-8">
-      <h3 class="mb-5">
-        Téma:
-      </h3>
-      <input-checkbox
-        v-for="option in filters.topics"
-        :key="option.key"
-        :value="value"
-        :option="option"
-        class="mr-12 py-1"
-        @input="onInput"
-      />
-    </div>
-    <div class="mr-12 mb-8">
-      <h3 class="mb-5">
-        Rola:
-      </h3>
-      <input-checkbox
-        v-for="option in filters.roles"
-        :key="option.key"
-        :value="value"
-        :option="option"
-        class="mr-12 py-1"
-        @input="onInput"
-      />
-    </div>
+    <DocumentsOverviewFiltersGroup
+      v-if="filters.types && filters.types.length > 0"
+      :filters="filters.types"
+      :value="value"
+      title="Typ dokumentu:"
+      @input="onInput"
+    />
+    <DocumentsOverviewFiltersGroup
+      v-if="filters.topics && filters.topics.length > 0"
+      :filters="filters.topics"
+      :value="value"
+      title="Téma:"
+      @input="onInput"
+    />
+    <DocumentsOverviewFiltersGroup
+      v-if="filters.roles && filters.roles.length > 0"
+      :filters="filters.roles"
+      :value="value"
+      title="Rola:"
+      @input="onInput"
+    />
     <ButtonClearFilters
-      v-show="value.length > 0"
+      v-show="value && value.length > 0"
       @click="onCancel"
     />
   </div>
 </template>
 
 <script>
-import InputCheckbox from './atoms/InputCheckbox'
 import ButtonClearFilters from './atoms/ButtonClearFilters'
+import DocumentsOverviewFiltersGroup from './DocumentsOverviewFiltersGroup'
 
 export default {
   components: {
-    InputCheckbox,
+    DocumentsOverviewFiltersGroup,
     ButtonClearFilters
   },
   props: {
---
+++ resources/js/components/DocumentsOverviewFiltersGroup.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mr-12 mb-8">
+    <h3 class="mb-5">
+      {{ title }}
+    </h3>
+    <input-checkbox
+      v-for="option in filters"
+      :key="option.key"
+      :value="value"
+      :option="option"
+      class="mr-12 py-1"
+      @input="$emit('input', $event)"
+    />
+  </div>
+</template>
+
+<script>
+import InputCheckbox from './atoms/InputCheckbox'
+
+export default {
+  components: {
+    InputCheckbox
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Array,
+      required: true
+    },
+    filters: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
