36d45faffb3f4de49f83c8e90939c60acb535b2c
Ernest Walzel <ewalzel@gmail.com>
2021-06-11 09:48:05 +0200

    Importing work media (work_pictures)

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Work extends Model
+class Work extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
+
+    public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection('images')
+            ->withResponsiveImages();
+    }
 }
