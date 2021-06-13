b8373781e7d0a6a21863ab2815f89c93682a197a
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-13 07:32:17 +0200

    Styling improvements to the artworks map popup

--- resources/js/components/ArtworksMapPopup.vue
+++ resources/js/components/ArtworksMapPopup.vue
@@ -1,5 +1,8 @@
 <template>
-  <div v-if="artwork && artwork.properties">
+  <div
+    v-if="artwork && artwork.properties"
+    class="p-2"
+  >
     <!-- TODO: add artwork preview image once available -->
     <h3>
       {{ artwork.properties.title }}
@@ -7,14 +10,22 @@
     <p>
       {{ artwork.properties.description }}
     </p>
-    <a :href="artwork.properties.url">
+    <LinkArrow
+      :url="artwork.properties.url"
+      class="mt-3"
+    >
       {{ __('buildings.building_more') }}
-    </a>
+    </LinkArrow>
   </div>
 </template>
 
 <script>
+import LinkArrow from './atoms/links/LinkArrow'
+
 export default {
+  components: {
+    LinkArrow
+  },
   props: {
     artwork: {
       type: Object,
