b5c34bfb81f4ca03ee9bf3d499264b80da6b6f7e
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-14 12:34:17 +0200

    Works link added to the map

--- resources/views/components/section-map.blade.php
+++ resources/views/components/section-map.blade.php
@@ -1,6 +1,6 @@
 <section class="mt-20">
     <x-heading-section>
-        <x-link-arrow :url="$titleUrl">
+        <x-link-arrow :url="$worksUrl">
             {{ __('buildings.architecture_register' )}}
         </x-link-arrow>
     </x-heading-section>
