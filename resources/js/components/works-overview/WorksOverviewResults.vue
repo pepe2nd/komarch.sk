e64570ffe965b8d8e31964db09381ff0f8d21039
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-14 12:31:33 +0200

    Basic results count added

--- resources/js/components/works-overview/WorksOverviewResults.vue
+++ resources/js/components/works-overview/WorksOverviewResults.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="flex flex-wrap">
-    <WorksOverviewResultsItem
-      v-for="item in results"
-      :key="item.id"
-      :item="item"
-    />
+  <div>
+    <div class="flex">
+      {{ count }}
+    </div>
+    <div class="flex flex-wrap">
+      <WorksOverviewResultsItem
+        v-for="item in results"
+        :key="item.id"
+        :item="item"
+      />
+    </div>
   </div>
 </template>
 
@@ -12,11 +17,19 @@
 import WorksOverviewResultsItem from './WorksOverviewResultsItem'
 
 export default {
-  components: { WorksOverviewResultsItem },
+  components: {
+    WorksOverviewResultsItem
+  },
   props: {
     results: {
       type: Array,
       required: true
+    }
+  },
+  computed: {
+    count () {
+      // TODO: add localization / plural forms
+      return `(${this.results.length}) Objektov`
     }
   }
 }
