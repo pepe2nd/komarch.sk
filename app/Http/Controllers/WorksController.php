f1b662e14e1dc125aa948e2938d4feb0ac0f00c9
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-15 17:32:30 +0200

    Controller adjusted to provide index and detail

--- app/Http/Controllers/WorksController.php
+++ app/Http/Controllers/WorksController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Work;
 
 class WorksController extends Controller
 {
     public function index(Request $request)
     {
-        return view('works');
+        return view('works.index');
+    }
+
+    public function show(Work $work)
+    {
+        return view('works.show', compact('work'));
     }
 }
---
+++ resources/views/works/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('title', 'správy')
+
+@section('content')
+
+@include('components.header.header')
+
+<div class="container mx-auto px-6">
+    Hello
+</div>
+
+<x-footer.footer></x-footer.footer>
+
+@endsection
