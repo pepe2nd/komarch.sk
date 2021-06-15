f8d61040c6570b4645e9b241094128cce177bb96
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-15 18:09:19 +0200

    Work detail header added (base)

---
+++ resources/views/components/work/work-header.blade.php
@@ -0,0 +1,5 @@
+<header class="container-narrow mx-auto">
+    <h1 class="text-xl tracking-tight leading-snug mt-4 lg:mt-14">
+        {{ $work->name }}
+    </h1>
+</header>
--- resources/views/works/show.blade.php
+++ resources/views/works/show.blade.php
@@ -10,7 +10,8 @@
 <x-header.header></x-header.header>
 <div class="container mx-auto px-6">
     <x-work.work-back :work="$work"></x-work.work-back>
-     <x-work.work-content :work="$work"></x-work.work-content>
+    <x-work.work-header :work="$work"></x-work.work-header>
+    <x-work.work-content :work="$work"></x-work.work-content>
 </div>
 <x-footer.footer></x-footer.footer>
 
