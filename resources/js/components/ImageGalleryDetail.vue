b891bf8056a316fcaaab0ced3fe8195da361fc3b
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-15 19:31:44 +0200

    Close button and enter / leave transtions for detail added

--- resources/js/components/ImageGalleryDetail.vue
+++ resources/js/components/ImageGalleryDetail.vue
@@ -4,7 +4,32 @@
       :src="image.src.replace('localhost', 'localhost:8000')"
       :alt="image.alt"
       class="h-80 mx-auto cursor-pointer"
+      @click="isDetailOpen = true"
     >
+    <transition
+      name="image-fade"
+      mode="in-out"
+    >
+      <div
+        v-if="isDetailOpen"
+        class="fixed inset-0 z-10 flex items-center justify-center p-6"
+        @click="isDetailOpen = false"
+      >
+        <div class="bg-black opacity-70 absolute inset-0" />
+        <div class="max-w-screen-lg max-h-full w-full relative">
+          <img
+            :src="image.src.replace('localhost', 'localhost:8000')"
+            :alt="image.alt"
+          >
+          <button
+            class="w-10 h-10 absolute top-0 right-0 text-xl text-white focus:outline-none"
+            @click="isDetailOpen = false"
+          >
+            X
+          </button>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -15,6 +40,23 @@ export default {
       type: Object,
       required: true
     }
+  },
+  data () {
+    return {
+      isDetailOpen: false
+    }
   }
 }
 </script>
+
+<style>
+.image-fade-enter-active,
+.image-fade-leave-active {
+  transition: opacity 300ms;
+}
+
+.image-fade-enter,
+.image-fade-leave {
+  opacity: 0
+}
+</style>
