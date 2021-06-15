c77f9c43159236fc31cbe0f2c25e8497ad86f6bf
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-15 19:01:23 +0200

    ImageGallery props populated with mock images

--- resources/js/components/ImageGallery.vue
+++ resources/js/components/ImageGallery.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="mt-16">
-    Image Gallery
+    <img
+      v-for="(image, index) in images"
+      :key="index"
+      :src="image.src.replace('localhost', 'localhost:8000')"
+      :alt="image.alt"
+    >
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    images: {
+      type: Array,
+      default: () => [
+        { src: 'http://localhost/storage/35/1.jpg', alt: 'Image' },
+        { src: 'http://localhost/storage/41/CEZAAR2020-01.jpg', alt: 'Image' },
+        { src: 'http://localhost/storage/47/Malovcova-(2).jpg', alt: 'Image' },
+        { src: 'http://localhost/storage/51/A3UMFIT_DSC8312.jpg', alt: 'Image' },
+        { src: 'http://localhost/storage/57/_vmo5540-2.jpg', alt: 'Image' }
+      ]
+    }
+  }
+}
+</script>
