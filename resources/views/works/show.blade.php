b18a626aa6ad11859010e6de1ddc4e176c743d17
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-15 18:03:59 +0200

    Work detail content added

--- resources/views/works/show.blade.php
+++ resources/views/works/show.blade.php
@@ -10,7 +10,7 @@
 <x-header.header></x-header.header>
 <div class="container mx-auto px-6">
     <x-work.work-back :work="$work"></x-work.work-back>
-    {{ $work }}
+     <x-work.work-content :work="$work"></x-work.work-content>
 </div>
 <x-footer.footer></x-footer.footer>
 
