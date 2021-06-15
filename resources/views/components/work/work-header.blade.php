19e252fe505179c70396bfb62aee716d2adb360f
Andrej Bilec <andrej.bilec@gmail.com>
2021-06-15 18:48:42 +0200

    Work detail tags added

--- resources/views/components/work/work-header.blade.php
+++ resources/views/components/work/work-header.blade.php
@@ -1,5 +1,20 @@
 <header class="container-narrow mx-auto">
 
+    <div class="flex mt-4 lg:mt-14">
+        @foreach ($work->awards as $award)
+            <x-tag-hash :url="''">
+                {{ $award->name }}
+            </x-tag-hash>
+        @endforeach
+        <x-tag-hash :url="''">
+            {{ $work->location_city }}
+        </x-tag-hash>
+
+        <x-tag-hash :url="''">
+            {{ $work->date_design_start }}
+        </x-tag-hash>
+    </div>
+
     <div class="flex items-center space-between mt-4 lg:mt-14">
         <h1 class="text-xl tracking-tight leading-snug">{{ $work->name }}</h1>
         <x-share-bar></x-share-bar>
