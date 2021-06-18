37d81efe1c1c9cbed4d1175edc96f24759793bae
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-06-18 18:26:05 +0200

    add translations to home page

--- resources/views/components/header/header-logo.blade.php
+++ resources/views/components/header/header-logo.blade.php
@@ -1,4 +1,4 @@
 <!-- TODO: replace with a proper logo once available -->
 <a href="{{ route('home') }}">
-    <img src="{{ asset('images/ska_logo.svg') }}" alt="Slovenská Komora Architektov">
+    <img src="{{ asset('images/ska_logo.svg') }}" alt="{{ __('app.title') }}">
 </a>
