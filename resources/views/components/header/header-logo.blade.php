a955f58d71fe592af8037321b5fcb1eb4fbfc3be
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-06-18 18:10:43 +0200

    use SKA logo in SVG

--- resources/views/components/header/header-logo.blade.php
+++ resources/views/components/header/header-logo.blade.php
@@ -1,9 +1,4 @@
 <!-- TODO: replace with a proper logo once available -->
 <a href="{{ route('home') }}">
-    <span class="block">
-        S &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        K &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        A
-    </span>
-    <span class="block mt-5">Slovenská Komora Architektov</span>
+    <img src="{{ asset('images/ska_logo.svg') }}" alt="Slovenská Komora Architektov">
 </a>
