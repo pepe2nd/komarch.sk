158ebac5143420400587b3c7f8f7265581f51574
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-06-18 22:40:05 +0200

    add translation strings

--- resources/lang/sk/works.php
+++ resources/lang/sk/works.php
@@ -4,6 +4,7 @@ return [
     'search_placeholder' => 'Architekt, architektka, dielo, ...',
     'awards' => 'Ocenenia',
     'realisation_year' => 'Rok realizácie',
+    'design_year' => 'Rok vzniku',
     'objects_one' => 'Objekt',
     'objects_few' => 'Objekty',
     'objects_plural' => 'Objektov',
@@ -13,4 +14,10 @@ return [
     'private' => 'Súkromný',
     'works_register' => 'Register diel',
     'intro' => 'Podporujeme rozvoj architektúry na Slovensku. Sme odbornou organizáciou, ktorá sa zameriava na vzdelávanie a informovanie verejnosti o potrebách kvalitnej architektúry.',
+    'typology_of_the_work' => 'Typológia diela',
+    'address' => 'Adresa',
+    'awards' => 'Ocenenia',
+    'authors' => 'Autori/Autorky',
+    'about_the_work' => 'O diele',
+    'literature' => 'Literatúra',
 ];
--- resources/views/components/work/work-content.blade.php
+++ resources/views/components/work/work-content.blade.php
@@ -1,21 +1,21 @@
 <div class="post-content mx-auto mt-10">
     @if ($work->date_design)
         <div class="mb-9">
-            <div class="mb-1">Rok vzniku:</div>
+            <div class="mb-1">{{ __('works.design_year') }}:</div>
             {{ $work->date_design }}
         </div>
     @endif
 
     @if ($work->date_construction)
         <div class="mb-9">
-            <div class="mb-1">Rok realizácie:</div>
+            <div class="mb-1">{{ __('works.realisation_year') }}:</div>
             {{ $work->date_construction }}
         </div>
     @endif
 
     @if ($work->functions->count() > 0)
         <div class="mb-9">
-            <div class="mb-1">Typológia diela:</div>
+            <div class="mb-1">{{ __('works.typology_of_the_work') }}:</div>
             @foreach ($work->functions as $function)
                 <x-link-arrow url="{{ route('works', ['function' => $function->name]) }}">
                     {{ $function->name }}
@@ -26,14 +26,14 @@
 
     @if ($work->location)
         <div class="mb-9">
-            <div class="mb-1">Adresa:</div>
+            <div class="mb-1">{{ __('works.address') }}:</div>
             {{ $work->location }}
         </div>
     @endif
 
     @if ($work->awards->count() > 0)
         <div class="mb-9">
-            <div class="mb-1">Ocenenia:</div>
+            <div class="mb-1">{{ __('works.awards') }}:</div>
             @foreach ($work->awards as $award)
                 <x-link-arrow url="{{ route('works', ['award' => $award->name]) }}">
                     {{ $award->name }}
@@ -44,20 +44,20 @@
 
     @if ($work->other_architects->count() > 0)
         <div class="mb-9">
-            <div class="mb-1">Autori/Autorky:</div>
+            <div class="mb-1">{{ __('works.authors') }}:</div>
             {{ $work->other_architects->implode('name', ', ') }}
         </div>
     @endif
 
 
     <div class="mb-9">
-        <div class="mb-1">O diele:</div>
+        <div class="mb-1">{{ __('works.about_the_work') }}:</div>
         {!! nl2br($work->annotation) !!}
     </div>
 
     @if ($work->citationPublications->count() > 0)
         <div class="mb-9">
-            <div class="mb-1">Literatúra:</div>
+            <div class="mb-1">{{ __('works.literature') }}:</div>
             @foreach ($work->citationPublications as $publication)
                 <x-link-arrow url="{{ route('works', ['publication' => $publication->publication_name]) }}">
                     {{ $publication->publication_name }}
