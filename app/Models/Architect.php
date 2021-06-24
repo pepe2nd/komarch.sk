865d7cd0104ab0545fe6f0606e89efeee80c0c8e
Ernest Walzel <ewalzel@gmail.com>
2021-06-24 14:22:43 +0200

    Architect & Address models

---
+++ app/Models/Architect.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Staudenmeir\EloquentHasManyDeep\HasRelationships as HasDeepRelationships;
+
+class Architect extends Model
+{
+    use HasFactory;
+    use HasDeepRelationships;
+
+
+    public function addresses()
+    {
+        return $this->hasMany(Address::class);
+    }
+
+    // Note this includes both nominations and awarded awards
+    public function awards()
+    {
+        return $this->hasManyDeep(Award::class, ['architect_work', Work::class, 'award_work']);
+    }
+
+    public function works()
+    {
+        return $this->belongsToMany(Work::class);
+    }
+}
