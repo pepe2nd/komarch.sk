43c1e3dd4fa49bf9806d05c8f4d4283fac9082c4
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-07-06 18:38:07 +0200

    contest detail with necessary components

--- resources/lang/sk/contests.php
+++ resources/lang/sk/contests.php
@@ -2,6 +2,7 @@
 
 return [
     'title' => 'Súťaže',
+    'navigate_back' => 'Späť na všetky súťaže',
     'no_results' => 'Nenašli sa žiadne súťaže',
     'typology' => 'Typológia súťaže'
 ];
--- resources/views/contests/show.blade.php
+++ resources/views/contests/show.blade.php
@@ -11,11 +11,10 @@
 
 @section('content')
 <x-header.header></x-header.header>
-{{-- <div class="container mx-auto px-6">
+<div class="container mx-auto px-6">
     <x-contest.contest-back :contest="$contest"></x-contest.contest-back>
     <x-contest.contest-header :contest="$contest"></x-contest.contest-header>
     <x-contest.contest-content :contest="$contest"></x-contest.contest-content>
-    <x-contest.contest-related :contest="$contest"></x-contest.contest-related>
-</div> --}}
+</div>
 <x-footer.footer></x-footer.footer>
 @stop
