7415c39d1cb6f47ba8a7d0d8ffafd753c87ecaff
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-07-19 17:10:21 +0200

    add link to detail in cotests list

---
+++ resources/js/components/atoms/links/LinkArrowHover.vue
@@ -0,0 +1,27 @@
+<template>
+  <a
+    :href="url"
+    class="group block leading-relaxed hover:text-blue"
+  >
+    <slot />
+    <span
+      class="opacity-0 inline-block transform group-hover:opacity-100 group-hover:translate-x-3 duration-200"
+      :class="external ? 'icon-arrow-r' : 'icon-arrow-r-long'"
+    />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    external: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
