40962940f39e08b22a0c6e4adae5c24a94e15ac6
Ernest Walzel <ewalzel@gmail.com>
2021-08-05 17:32:54 +0200

    Show architects in works list

--- resources/js/components/TeaserWorkBig.vue
+++ resources/js/components/TeaserWorkBig.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="py-10">
     <div class="relative group">
-      <!-- TODO: remove the replacement of localhost -->
       <img
         v-if="item.cover_image"
+        ref="img"
         :srcset="item.cover_image.srcset"
         :alt="item.name"
         class="rounded-2xl"
@@ -11,7 +11,6 @@
         :src="item.cover_image.url"
         :width="item.cover_image.width"
         :height="item.cover_image.height"
-        ref="img"
       >
       <h3 class="mt-4 group-hover:text-blue">
         <a
@@ -31,6 +30,24 @@
         {{ filter }}
       </TagHash>
     </div>
+    <div class="flex flex-wrap mt-4">
+      <a
+        v-for="architect in item.architects"
+        :key="architect.id"
+        :href="architect.url"
+        class="group inline-block leading-relaxed text-blue mr-2"
+      >
+        {{ architect.first_name }} {{ architect.last_name }}
+        <span class="inline-block transform group-hover:translate-x-1 group-hover:-translate-y-1 duration-200 icon-arrow-tr" />
+      </a>
+      <span
+        v-for="architect in item.other_architects"
+        :key="architect.slug"
+        class="group inline-block leading-relaxed mr-2"
+      >
+        {{ architect.name }}
+      </span>
+    </div>
   </div>
 </template>
 
@@ -48,18 +65,16 @@ export default {
       required: true
     }
   },
+  mounted () {
+    if (this.item.cover_image) {
+      const size = this.$refs.img.getBoundingClientRect().width
+      this.$refs.img.sizes = Math.ceil(size / window.innerWidth * 100) + 'vw'
+    }
+  },
   methods: {
     onTagClicked (filter) {
-      console.log(filter)
-    },
-  },
-  mounted () {
-    // @TODO: try this.$nextTick or anything nicer then this
-    setTimeout(() => {
-      let size=this.$refs.img.getBoundingClientRect().width
-      this.$refs.img.sizes = Math.ceil(size/window.innerWidth*100)+'vw';
-    }, 500)
+      console.log(filter) // TODO
+    }
   }
-
 }
 </script>
