7c72ee122582debff3a49ecfbd28801a5fc47b9c
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-08-17 23:31:36 +0200

    remove duplicated "published" column in crud

--- app/Http/Controllers/Admin/VideoCrudController.php
+++ app/Http/Controllers/Admin/VideoCrudController.php
@@ -76,8 +76,6 @@ class VideoCrudController extends CrudController
      */
     protected function setupListOperation()
     {
-        CRUD::setFromDb(); // columns
-
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
