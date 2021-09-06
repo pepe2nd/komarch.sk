bc170809d1268162f3f0e74194a968c69d59e09b
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-09-06 17:19:19 +0200

    highlight all parent pages in navigation

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -16,7 +16,7 @@
     <div id="nav-submenu" class="md:flex">
         @foreach ($page->breadcrumbs as $i => $breadcrumb)
             <div class="md:mr-16 lg:mr-24 mb-7 md:mb-0">
-                <x-header.header-navigation-list :items="$breadcrumb->children" />
+                <x-header.page-navigation-list :items="$breadcrumb->children" :activeItem="$page" />
             </div>
         @endforeach
     </div>
