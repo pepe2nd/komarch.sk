bfd5c911943b95b0964deb36ae3e44aa0a53a8c5
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-09-09 14:41:13 +0200

    show preview in lightbox

--- resources/js/components/documents-overview/DocumentsOverviewResultsPreview.vue
+++ resources/js/components/documents-overview/DocumentsOverviewResultsPreview.vue
@@ -1,12 +1,42 @@
 <template>
-  <a
-    :href="document.file.thumb"
-    class="focus:outline-none hover:text-blue"
-    target="_blank"
-  >
-    <!-- TODO: add icon when available -->
-    Preview
-  </a>
+  <div>
+    <a
+      :href="document.file.thumb"
+      class="focus:outline-none hover:text-blue"
+      target="_blank"
+      @click.prevent.stop="isDetailOpen = true"
+    >
+      <!-- TODO: add icon when available -->
+      Preview
+    </a>
+
+    <transition
+      name="image-fade"
+      mode="in-out"
+    >
+      <div
+        v-if="isDetailOpen"
+        class="fixed inset-0 z-10 flex items-center justify-center p-6"
+      >
+        <div
+          class="bg-black opacity-70 absolute inset-0 cursor-zoom-out"
+          @click="isDetailOpen = false"
+        />
+        <div class="max-w-screen-lg max-h-full w-full relative">
+          <img
+            class="object-contain h-1/2-screen m-auto"
+            :src="document.file.thumb"
+          >
+          <button
+            class="w-14 h-14 absolute top-0 right-0 focus:outline-none flex items-center justify-center"
+            @click="isDetailOpen = false"
+          >
+            <span class="icon-close text-2xl text-white" />
+          </button>
+        </div>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -15,6 +45,11 @@ export default {
     document: {
       type: Object,
       required: true
+    }
+  },
+  data () {
+    return {
+      isDetailOpen: false
     }
   }
 }
