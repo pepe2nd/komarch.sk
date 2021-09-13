e07e58233f5a56c682e829c9d0aed3fe8b142ae9
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-09-13 11:26:28 +0200

    fix spacings in works

--- resources/js/components/TeaserWorkBig.vue
+++ resources/js/components/TeaserWorkBig.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-10">
+  <div class="py-10 mb-10">
     <div class="relative group">
       <img
         v-if="item.cover_image"
@@ -12,7 +12,7 @@
         :width="item.cover_image.width"
         :height="item.cover_image.height"
       >
-      <h3 class="mt-4 group-hover:text-blue">
+      <h3 class="mt-4 group-hover:text-blue leading-normal">
         <a
           :href="item.url"
           class="link-area"
@@ -33,7 +33,7 @@
         v-for="architect in item.architects"
         :key="architect.id"
         :href="architect.url"
-        class="group inline-block leading-relaxed text-blue mr-2"
+        class="group inline-block leading-normal text-blue mr-2"
       >
         {{ architect.first_name }} {{ architect.last_name }}
         <span class="inline-block transform group-hover:translate-x-1 group-hover:-translate-y-1 duration-200 icon-arrow-tr" />
@@ -41,7 +41,7 @@
       <span
         v-for="architect in item.other_architects"
         :key="architect.slug"
-        class="group inline-block leading-relaxed mr-2"
+        class="group inline-block leading-normal mr-2"
       >
         {{ architect.name }}
       </span>
