878c98d37240cb8619b8a0850efffb50e864b5a8
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-09-13 12:04:56 +0200

    fix range slider + labels sizing on works filters

--- resources/js/components/atoms/buttons/ButtonClearFilters.vue
+++ resources/js/components/atoms/buttons/ButtonClearFilters.vue
@@ -1,9 +1,9 @@
 <template>
   <button
-    class="mt-5 focus:outline-none hover:text-blue"
+    class="mt-5 focus:outline-none hover:text-blue text-sm"
     v-on="$listeners"
   >
-    <span class="icon-close text-sm mr-1" />
-    {{ __('generic.cancel_filters') }}
+    <span class="icon-close text-lg mr-1" />
+    <span>{{ __('generic.cancel_filters') }}</span>
   </button>
 </template>
