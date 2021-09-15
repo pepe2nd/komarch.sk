98929fd986e0a56996f777265fae756bbb35865b
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-09-15 21:35:09 +0200

    show download size in human readable format on hover

--- resources/js/components/documents-overview/DocumentsOverviewResultsDownload.vue
+++ resources/js/components/documents-overview/DocumentsOverviewResultsDownload.vue
@@ -4,7 +4,7 @@
     :download="document.file.name"
     class="focus:outline-none hover:text-blue"
     target="_blank"
-    :title="__('documents.download')"
+    :title="downloadTitle"
   >
     <span class="pl-2 text-lg icon-download"></span>
   </a>
@@ -17,6 +17,26 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    downloadTitle() {
+      return this.__('documents.download') + ' (' + this.formatBytes(this.document.file.size) + ')'
+    }
+  },
+  methods: {
+    formatBytes(bytes, decimals = 2) {
+        if (bytes === 0) return '0 Bytes';
+
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
+
   }
+
 }
 </script>
