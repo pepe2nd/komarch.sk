6ead580bbae7b1bedba5282a72ce2c8810bd25e2
Peter Krajčovič <69574333+pepe2nd@users.noreply.github.com>
2021-09-16 19:25:01 +0200

    update documents.php
    changed title and perex before launch

--- resources/lang/sk/documents.php
+++ resources/lang/sk/documents.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'title' => 'Legislatíva a dokumenty',
-    'perex' => '',
+    'title' => 'Právne normy a dokumenty',
+    'perex' => 'Komora usiluje o partnerské vzťahy s ministerstvami a inými ústrednými orgánmi, vďaka čomu je prítomná pri tvorbe právnych predpisov a odvetvových rozvojových koncepcií pre oblasť architektúry, urbanizmu a životného prostredia.',
     'preview' => 'Náhľad',
     'download' => 'Stiahnuť',
     'no_documents' => 'Nenašli sa žiadne dokumenty',
