8b246da7d0b8b1b4b85f12766eefbb4ecb641b27
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-09-17 00:12:04 +0200

    layout for error pages (404/500/...)

--- resources/lang/sk/app.php
+++ resources/lang/sk/app.php
@@ -5,4 +5,5 @@ return [
     'description' => 'Slovenská komora architektov je samosprávne profesné združenie, ktoré bolo zriadené zákonom 1. júna 1992. Bola založená pre plnenie štátnej regulácie povolaní architekt a krajinný architekt. Zabezpečuje ich autorizáciu, vedie zoznamy a vykonáva dohľad nad ich výkonmi.',
     'intro' => 'Podporujeme rozvoj architektúry na Slovensku. Sme odbornou organizáciou, ktorá sa zameriava na vzdelávanie a informovanie verejnosti o potrebách kvalitnej architektúry.',
     'search_placeholder' => 'Architekt / architektka, dielo, dokument, iné ...',
+    'return_home' => 'Návrat domov',
 ];
