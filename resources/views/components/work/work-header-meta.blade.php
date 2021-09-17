5adfa2c0ce81e6e9a8d163e305e04f8fcfa4c080
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-09-17 13:40:23 +0200

    fix tags in work detail

--- resources/views/components/work/work-header-meta.blade.php
+++ resources/views/components/work/work-header-meta.blade.php
@@ -1,14 +1,14 @@
 <div class="flex mt-14">
     @foreach ($work->awards as $award)
-        <x-tag-hash :url="''">
+        <x-tag-hash :url="route('works', ['awards[]' => $award->name])">
             {{ $award->name }}
         </x-tag-hash>
     @endforeach
-    <x-tag-hash :url="''">
-        {{ $work->location_city }}
+    <x-tag-hash :url="route('works', ['location_districts[]' => $work->location_district])">
+        {{ $work->location_district }}
     </x-tag-hash>
 
-    <x-tag-hash :url="''">
+    <x-tag-hash :url="route('works', ['year_from[]' => $work->date_design_start])">
         {{ $work->date_design_start }}
     </x-tag-hash>
 </div>
