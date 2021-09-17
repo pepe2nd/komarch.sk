02ac4d82b6c1a1d1cf2b2ae1738e30f9d9883fa8
Ernest Walzel <ewalzel@gmail.com>
2021-09-17 13:30:23 +0200

    Prevent hidden filters from 'jumping' when empty

--- resources/js/components/works-overview/WorksOverviewFilters.vue
+++ resources/js/components/works-overview/WorksOverviewFilters.vue
@@ -1,38 +1,33 @@
 <template>
   <div class="md:flex items-start">
     <InputCheckboxGroup
-      v-if="filters.typologies && filters.typologies.length > 0"
-      :filters="filters.typologies"
+      :filters="filters.typologies || []"
       :value="value"
       :title="`${__('works.typology')}:`"
       @input="onInput"
     />
     <InputCheckboxGroup
-      v-if="filters.location_districts && filters.location_districts.length > 0"
-      :filters="filters.location_districts"
+      :filters="filters.location_districts || []"
       :value="value"
+      :cols="2"
       :title="`${__('works.location_districts')}:`"
-      @input="onInput"
-      :cols=2
       class="flex-grow w-72"
+      @input="onInput"
     />
     <InputCheckboxGroup
-      v-if="filters.awards && filters.awards.length > 0"
-      :filters="filters.awards"
+      :filters="filters.awards || []"
       :value="value"
       :title="`${__('works.awards')}:`"
       @input="onInput"
     />
     <InputCheckboxGroup
-      v-if="filters.investors && filters.investors.length > 0"
-      :filters="filters.investors"
+      :filters="filters.investors || []"
       :value="value"
       :title="`${__('works.investor')}:`"
       @input="onInput"
     />
     <InputCheckboxGroup
-      v-if="filters.citations && filters.citations.length > 0"
-      :filters="filters.citations"
+      :filters="filters.citations || []"
       :value="value"
       :title="`${__('works.citations')}:`"
       @input="onInput"
