64ca789648ec622e7cef85dc41e11a0e74bffda3
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-09-27 14:51:16 +0200

    use random SKA logo

--- resources/views/components/header/header-logo.blade.php
+++ resources/views/components/header/header-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ route('home') }}" class="text-center md:order-1">
-    <img src="{{ asset('images/ska_logo.svg') }}" alt="{{ __('app.title') }}" class="w-full md:w-auto my-20 md:my-0">
+    <img src="{{ asset('images/logo/logo_SKA_'.rand(0,4).'.svg') }}" alt="{{ __('app.title') }}" class="w-full md:w-auto my-20 md:my-0">
 </a>
