b984bf87fe8abddb7bf4276898ba506f27a0409c
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-07-15 13:02:11 +0200

    show submenu in contests index

--- resources/views/contests/index.blade.php
+++ resources/views/contests/index.blade.php
@@ -11,6 +11,14 @@
 @include('components.header.header')
 
 <div class="container mx-auto px-6">
+    <div id="nav-submenu" class="md:flex">
+        @foreach ($page->breadcrumbs as $i => $breadcrumb)
+            <div class="md:mr-16 lg:mr-24 mb-7 md:mb-0 pl-{{$i * 4}} md:pl-0">
+                <x-header.page-navigation-list :items="$breadcrumb->children" :activeItem="$page" />
+            </div>
+        @endforeach
+    </div>
+
     <x-intro
         :title="trans('contests.title')"
         :text="trans('contests.perex')"
