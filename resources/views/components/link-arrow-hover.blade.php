cee5a06f54f67fbcacc8d09c92f69994ad1258b3
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-02-15 22:00:06 +0100

    KOMARCH-254 fix arrow icon on new line

--- resources/views/components/link-arrow-hover.blade.php
+++ resources/views/components/link-arrow-hover.blade.php
@@ -1,11 +1,10 @@
 @php
-    $class = 'group block leading-relaxed hover:text-blue';
+    $class = 'group block leading-relaxed hover:text-blue pr-5';
     $class .= (url()->current() == $url) ? ' text-blue' : '';
 @endphp
 <a
     href="{{ $url }}"
     {{ $attributes->merge(['class' => $class]) }}
 >
-    {{ $slot }}
-    <span class="opacity-0 inline-block transform group-hover:opacity-100 group-hover:translate-x-3 duration-200 {{ $external ?? false ? 'icon-arrow-r' : 'icon-arrow-r-long' }}"></span>
+    {{ $slot }}&nbsp;<span class="opacity-0 inline-block transform group-hover:opacity-100 group-hover:translate-x-3 duration-200 {{ $external ?? false ? 'icon-arrow-r' : 'icon-arrow-r-long' }}"></span>
 </a>
