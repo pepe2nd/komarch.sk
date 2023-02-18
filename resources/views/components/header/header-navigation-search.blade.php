7dee49dd66998dcc3a739fe453b4e1243633c43b
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-02-18 12:40:46 +0100

    remove placeholder from search in navigation

--- resources/views/components/header/header-navigation-search.blade.php
+++ resources/views/components/header/header-navigation-search.blade.php
@@ -1,4 +1,4 @@
 <form action="{{ route('posts.index') }}" autocomplete="off" {{ $attributes->merge(['class' => 'w-72 flex align-center']) }}>
-    <input-search placeholder="{{ __('app.search_placeholder') }}"></input-search>
+    <input-search></input-search>
     <span class="icon-search ml-1 text-lg"></span>
 </form>
