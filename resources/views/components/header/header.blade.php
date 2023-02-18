3d9e2243ad7b81ad5cedbe12bb54fbc24c18f423
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-02-18 12:38:45 +0100

    update header to show top 4 menu items in a row

--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -1,7 +1,19 @@
-<header class="container mx-auto px-6 md:px-6 pt-2 md:py-12 lg:flex lg:items-start">
-    <x-header.header-navigation
-        :navItems="\App\Models\Page::topMenu()->get()"
-    />
-    <x-header.header-langswitch />
-    <x-header.header-logo />
+<header class="container mx-auto px-6 pt-2 md:py-12 flex items-end justify-between w-full">
+    <a href="{{ route('home') }}" class="flex-none">
+        <img src="{{ asset('images/logo/logo_SKA_' . rand(0, 4) . '.svg') }}" alt="{{ __('app.title') }}" class="w-64">
+    </a>
+    <div id="nav-content" class="nav-menu md:flex">
+        <x-header.header-navigation-search class="mr-10" />
+
+        <ul class="flex space-x-3">
+            @foreach (\App\Models\Page::topMenu()->limit(4)->get() as $item)
+                <li>
+                    <x-link-arrow-hover class="whitespace-nowrap" :url="$item->url">
+                        {{ $item->title }}
+                    </x-link-arrow-hover>
+                </li>
+            @endforeach
+        </ul>
+
+    </div>
 </header>
