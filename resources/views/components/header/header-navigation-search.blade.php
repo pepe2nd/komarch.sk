9cc4fbd08a3364a41ac390fe332b5d318cf44211
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-02-18 13:50:01 +0100

    add toggle button

--- resources/views/components/header/header-navigation-search.blade.php
+++ resources/views/components/header/header-navigation-search.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ route('posts.index') }}" autocomplete="off" {{ $attributes->merge(['class' => 'w-72 flex align-center']) }}>
+<form action="{{ route('posts.index') }}" autocomplete="off" {{ $attributes->merge(['class' => 'w-64 flex align-center']) }}>
     <input-search></input-search>
     <span class="icon-search ml-1 text-lg"></span>
 </form>
--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -5,7 +5,7 @@
     <div id="nav-content" class="nav-menu md:flex">
         <x-header.header-navigation-search class="mr-10" />
 
-        <ul class="flex space-x-3">
+        <ul class="flex space-x-2">
             @foreach (\App\Models\Page::topMenu()->limit(4)->get() as $item)
                 <li>
                     <x-link-arrow-hover class="whitespace-nowrap" :url="$item->url">
@@ -13,6 +13,15 @@
                     </x-link-arrow-hover>
                 </li>
             @endforeach
+            <li>
+                <button type="button" class="w-8 h-7 relative focus:outline-none bg-white bottom-1 hover:text-blue">
+                    <span aria-hidden="true"
+                        class="block absolute h-px mb-0.5 w-full bg-current transform -translate-y-1.5"></span>
+                    <span aria-hidden="true" class="block absolute  h-px mb-0.5 w-full bg-current   transform"></span>
+                    <span aria-hidden="true"
+                        class="block absolute  h-px w-full bg-current transform  translate-y-1.5"></span>
+                </button>
+            </li>
         </ul>
 
     </div>
