0fa93d0d756c308844da174ab960637d65e7160d
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-08-10 16:40:00 +0200

    remove link-arrow-back blade component

--- resources/views/components/link-arrow-back.blade.php
+++
@@ -1,7 +0,0 @@
-<a
-    href="{{ $url }}"
-    {{ $attributes->merge(['class' => 'group block leading-relaxed hover:text-blue']) }}
->
-    <span class="inline-block transform group-hover:-translate-x-2 duration-200 {{ $external ?? false ? 'icon-arrow-l' : 'icon-arrow-l-long' }}"></span>
-    {{ $slot }}
-</a>
