c015944547d2453b35a42007c272cbefc264c380
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-09-19 11:13:10 +0200

    add command komarch:search:import

---
+++ app/Console/Commands/ImportSearchableEntitiesCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Post;
+use App\Models\Work;
+use App\Models\Contest;
+use App\Models\Architect;
+use Illuminate\Console\Command;
+
+class ImportSearchableEntitiesCommand extends Command
+{
+    private const SEARCHABLE_ENTITIES = [
+        Post::class,
+        Architect::class,
+        Work::class,
+        Contest::class,
+    ];
+
+    protected $signature = 'komarch:search:import';
+    protected $description = 'Import all searchable entities with Scout';
+
+    public function handle(): int
+    {
+        foreach (self::SEARCHABLE_ENTITIES as $entity) {
+            $this->call('scout:import', ['model' => $entity]);
+        }
+
+        return self::SUCCESS;
+    }
+}
