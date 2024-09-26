ed0b750214fd44b2e9282f064e965231e3c533b0
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-09-26 11:46:39 +0200

    update test_and_deploy action to set explicit TNTSEARCH_STORAGE

--- config/scout.php
+++ config/scout.php
@@ -104,7 +104,7 @@ return [
     ],
 
     'tntsearch' => [
-        'storage'  => storage_path(), //place where the index files will be stored
+        'storage'  => env('TNTSEARCH_STORAGE', storage_path()), //place where the index files will be stored)
         'fuzziness' => env('TNTSEARCH_FUZZINESS', false),
         'fuzzy' => [
             'prefix_length' => 2,
