1c17a1244dafea4122205a74cd9aec3d13e803dc
DiLionnele <jana@sefc.sk>
2025-10-06 15:06:31 +0200

    Updated styleguide.blade.php with the new texts and styles.

--- resources/views/styleguide.blade.php
+++ resources/views/styleguide.blade.php
@@ -1,201 +1,180 @@
-@php
-  $tags = Spatie\Tags\Tag::factory(5)->make();
-
-  $post = App\Models\Post::inRandomOrder()->first();
-  if (empty($post)) {
-      $post = App\Models\Post::factory()->rich()->hasTags(3)->make();
-      $post->generateSlug();
-  }
-@endphp
-
 @extends('layouts.app')
-
-@section('title')
-  Style Guide
-  @parent
-@endsection
+@section('title', 'Style Guide')
+<meta name="robots" content="noindex, nofollow">
 
 @section('content')
 
-<main class="container mx-auto">
-<h1>Style Guide</h1>
+@include('components.header.header')
 
+<main class="container mx-auto">
 <div>
 <div class="row">
-<section id="styleguide-branding" class="col m-1">
-  <h2>Branding</h2>
+    <section class="col m-10 mb-20">
+        <h1 class="text-2xl tracking-tight leading-snug mt-4 lg:mt-14">
+            V prípade, ak člen*ka SKA dlhodobo nevykonáva činnosť autorizovaného architekta*tky/ autorizovaného krajinného architekta*tky, zvažuje odchod na materskú/ otcovskú dovolenku alebo do dôchodku, resp. sa nachádza v inej špecifickej situácii, odporúčame zvážiť:
+        </h1>
+        <p class="text-2xl tracking-tight leading-snug mt-6 lg:mt-6">A. Vyčiarknutie zo zoznamu AA/ KA</p>
+        <p class="text-2xl tracking-tight leading-snug mt-6 lg:mt-6">B. Pozastavenie činnosti</p>
+    </section>
 
-  <h1 class="ls-2">Slovenská Komora Architektúry</h1>
-  <!-- TODO: Icons and favicons -->
-</section>
+    <section class="border-t border-black m-10 mb-24">
+        <h1 class="text-2xl tracking-tight leading-snug mb-14 lg:mt-20">A. Vyčiarknutie zo zoznamov AA/ KA</h1>
 
-<section id="styleguide-colors" class="col m-1">
-  <h2>Colors</h2>
+        <ol>
+            <li>
+                Doručenie vyplnenej žiadosti<br>
+                O vyčiarknutie zo zoznamu autorizovaných architektov/ autorizovaných krajinných architektov je potrebné podľa §17 ods.1 písm. b) zákona o autorizovaných architektoch autorizovaných stavebných inžinieroch písomne požiadať.
+                <a href="#" url="#" external="external" class="group block leading-relaxed hover:text-blue">
+                    Žiadosť o vyčiarknutie
+                    <span class="inline-block transform group-hover:translate-x-2 duration-200 icon-arrow-r"></span>
+                </a>
+            </li>
+            <li>
+                Vrátenie dokumentov<br>
+                Vyčiarknutím zo zoznamu strácajú platnosť autorizačné osvedčenie a pečiatka, ktoré sú prílohou k žiadosti. Ich neodovzdanie je priestupkom.
+            </li>
+            <li>
+                Dátum ukončenia členstva
+                Dátum ukončenia členstva nemôže byť retrospektívny. Najskôr je možné žiadateľa*ľku vyčiarknuť dňom doručenia žiadosti s prílohami.
+            </li>
+            <li>
+                Vyrozumenie
+                Architekt*ka obdrží z úradu komory písomné oznámenie o vyčiarknutí zo zoznamu. Dňom vyčiarknutia žiadateľovi*ke zaniká oprávnenie na výkon povolania.
+            </li>
+        </ol>
+    </section>
 
-  <div class="row">
-    <div class="col m-2"><div id="styleguide-color-primary" class="styleguide-color"></div></div>
-    <div class="col m-2"><div id="styleguide-color-secondary" class="styleguide-color"></div></div>
-    <div class="col m-2"><div id="styleguide-color-success" class="styleguide-color"></div></div>
-    <div class="col m-2"><div id="styleguide-color-info" class="styleguide-color"></div></div>
-    <div class="col m-2"><div id="styleguide-color-warning" class="styleguide-color"></div></div>
-  </div>
-</section>
-</div>
-
-<div class="row">
-<section id="styleguide-typography" class="col m-1">
-  <h2>Typography</h2>
-  <div class="cd-box">
-    <h1>Heading, <span></span></h1>
-    <p>Aa - <span></span>. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptate distinctio reprehenderit, autem deleniti ad <a href="#0">voluptatum eaque</a>. Optio ea aperiam nisi distinctio nemo repellat, voluptate fugiat. Quidem neque illum, blanditiis!</p>
-  </div>
-</section>
-
-<section id="styleguide-buttons" class="col m-1">
-  <h2>Buttons</h2>
-  <div class="cd-box">
-    <button class="btn btn-outline-dark">Outlined</button>
-    <button class="btn btn-outline-dark btn-sm">Outlined sm</button>
-    <button class="btn btn-outline-dark btn-sm btn-with-icon-right">Outlined sm with right icon<span>&times;</span></button>
-  </div>
-</section>
-</div>
-
-<div class="row">
-<section id="styleguide-icons" class="col m-1">
-  <h2>Icons</h2>
-
-  <ul>
-    <li class="icon-menu" />
-    <li class="icon-plus" />
-    <li class="icon-check" />
-    <li class="icon-close" />
-    <li class="icon-search" />
-    <li class="icon-arrow-down" />
-    <li class="icon-arrow-up" />
-    <li class="icon-chevron-right" />
-    <li class="icon-chevron-left" />
-    <li class="icon-cube" />
-    <li class="icon-minus" />
-    <li class="icon-fullscreen" />
-    <li class="icon-chevron-up" />
-    <li class="icon-chevron-down" />
-  </ul>
-</section>
-
-<section id="styleguide-form" class="col m-1">
-  <h2>Form</h2>
-
-  <form>
-    <fieldset>
-      <input class="form-control  w-50" type="text" >
-      <input class="form-control  w-50 is-valid" type="text">
-      <input class="form-control  w-50 is-invalid" type="text">
-    </fieldset>
-   <fieldset>
-      <div class="cd-input-wrapper">
-        <div class="custom-control custom-radio">
-          <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input" />
-          <label class="custom-control-label" for="customRadio1">Toggle 1</label>
-        </div>
+    <section class="m-10">
+        <ul>
+            <li>voliť a odvolávať zástupcov komory a členky a členov disciplinárnej komisie, voliť a odvolávať zástupcov komory a členky a členov disciplinárnej komisie, voliť a odvolávať zástupcov komory a členky a členov disciplinárnej komisie,</li>
+            <li>zriaďovať úrad komory a určovať základné pravidlá jeho činnosti,</li>
+            <li>schvaľovať predpisy komory, etický, disciplinárny a volebný poriadok,</li>
+            <li>schvaľovať výšku príspevkov na činnosť orgánov komory a rozpočet komory,</li>
+            <li>určovať základné smery a rámcové pravidlá medzinárodnej spolupráce.</li>
+        </ul>
+    </section>
+    <section class="m-10 mono">
+      <div>
+          <p>Čo to pre mňa znamená?</p>
+          <p>Oprávnenie zaniká právnickej osobe, ktorá mala na základe tohto osvedčenia vykonaný zápis v Obchodnom registri.</p>
+          <p>Fyzická osoba musí požiadať o zánik činnosti na Štatistickom úrade (zánik formy SZČO - slobodné povolanie podľa zákona č.138/1992 v znení neskorších predpisov o autorizovaných architektoch a autorizovaných stavebných inžinieroch; zánik formy v Generickom registri RPO sprostredkuje úrad komory za manipulačný poplatok 15 Eur).</p>
+          <p>Je potrebné upovedomiť poisťovňu, v ktorej má osoba uzavreté profesijné poistenie, o ukončení členstva v komore.</p>
       </div>
-     <div class="cd-input-wrapper">
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="customCheck1" />
-          <label class="custom-control-label" for="customCheck1">Check 1</label>
-        </div>
-      </div>
-     <div class="cd-input-wrapper cd-select">
-        <select name="select-this" id="select-this" class="custom-select input-sm form-control form-control-sm">
-          <option value="0">Select</option>
-          <option value="1">Option 1</option>
-          <option value="2">Option 2</option>
-          <option value="2">Option 3</option>
-        </select>
-      </div>
-    </fieldset>
-  </form>
-</section>
+    </section>
+    <section class="m-10 mt-14">
+        <p>
+            Ectectate ari aliquatur rae eictio delenem quideli quatem quiam amus, omnimos aut quas expero tores eicimus
+            accumendebis renihilique min eos ma quas et adis corrum volores re magnam qui de sitatur aut qui re,
+            sunt poresed ea posandest, qui de cor rerferibus exerchi libeaquibusa dolento officae andipsamus quaecti
+            aeriani omnitiberae nos voloren disqui quatus illanis parciaspic tenisse rchiliquat alit eum quat.
+            Atet volut alibero rporro cume pos deliquae sitiae pla cum fugit accaecus ea dipsum enihili quiat.
+            Untectur molorro que vernam facil maio. Rem aut ad ut volest imet ipsam expliquis sum fugiatur alit doluptius
+            nobis diti cuptaqui sequi omniatem. Berum quia dolectus, ad ullorio. Itatusdae natur autam eum sequos
+            aut atem exeribus ame ea id mo voluptatur? Quiam ipiducia nos atem ium isquam, velessinus eum fugiatectur,
+            cus dem aut doluptatem hilicaest unti ulpa aut que vit eum que nobis experem quate volumenisti berci
+            blandipit rest prest acestem qui debis non nihiliqui dunt.
+        </p>
+    </section>
+    <section class="m-10 mb-24">
+        <p>
+          <a href="#" url="#" external="external" class="group block leading-relaxed hover:text-blue">
+              Ročenka SKA / Správa o činnosti 2021 - 2023
+              <span class="inline-block transform group-hover:translate-x-2 duration-200 icon-arrow-r"></span>
+          </a>
+        </p>
+        <p>
+          <a href="#" url="#" external="external" class="group block leading-relaxed hover:text-blue">
+              XXII. ZASADNUTIE VZ SKA (2023) / uznesenie
+              <span class="inline-block transform group-hover:translate-x-2 duration-200 icon-arrow-r"></span>
+          </a>
+        </p>
+        <p>
+          <a href="#" url="#" external="external" class="group block leading-relaxed hover:text-blue">
+              Štatút Slovenskej komory architektov
+              <span class="inline-block transform group-hover:translate-x-2 duration-200 icon-arrow-r"></span>
+          </a>
+        </p>
+    </section>
 </div>
-
-<div class="row">
-<section id="card-tags" class="col m-1">
-    <h2>Component: tags</h2>
-
-    <div class="cd-box">
-        @include('components.tags')
-    </div>
-
-    <div class="cd-box code lang-php hljs xml">
-        @@include('components.tags', ['tags' => $tags])
-    </div>
-</section>
-
 </div>
-
-<div class="row">
-
-<section id="card-article-big" class="col m-1">
-    <h2>Component: article-big</h2>
-
-    <div class="cd-box">
-        @include('components.article-big')
-    </div>
-
-    <div class="cd-box code lang-php hljs xml">
-        @@include('components.article-big', ['post' => $post])
-    </div>
-</section>
-
-<section id="card-article-small" class="col m-1">
-    <h2>Component: article-small</h2>
-
-    <div class="cd-box">
-        @include('components.article-small')
-    </div>
-
-    <div class="cd-box code lang-php hljs xml">
-        @@include('components.article-small', ['post' => $post])
-    </div>
-</section>
-
-</div>
-
-<div class="row">
-
-<section id="card-tile" class="col m-1">
-    <h2>Component: tile</h2>
-
-    <div class="cd-box">
-        <x-tile
-            title="Ako sa stať autorizovaným členom?"
-            url="#"
-        />
-    </div>
-
-    <div class="cd-box code lang-php hljs xml">
-        &lt;x-tile
-            title="Ako sa stať autorizovaným členom?"
-            url="#"
-        /&gt;
-    </div>
-</section>
-
-</div>
-
-</div>
-
 </main>
 
 @endsection
 
 @push('styles')
-  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <style>
+        /* --- lists --- */
+        ul {
+            list-style-type: none;     /* (disc, circle, square) */
+            margin-left: 0;
+            padding-left: 3.5em;
+            position: relative;
+        }
+        ul li {
+            position: relative;
+            text-indent: 0;
+        }
+        ul li::before {
+            content: "– ";
+            position: absolute;
+            left: -3.5em;
+            width: 2em;
+            text-align: left;
+            color: inherit;
+        }
+
+        ol {
+            list-style-type: none;           /* turn off the default counting style */
+            counter-reset: list-counter; /* initialise counter */
+            margin-left: 0;
+            padding-left: 2.5em;
+            position: relative;
+        }
+        ol li {
+            counter-increment: list-counter; /* add 1 to each <li> */
+            position: relative;
+            margin-bottom: 1.3em;
+            text-indent: 0;
+        }
+        ol li::before {
+            content: "(" counter(list-counter) ") ";
+            position: absolute;
+            left: -2.5em;
+            width: 2em;
+            text-align: left;
+            color: inherit;
+        }
+        ol li a {
+            padding-top: 1.3em;
+            margin-left: -2.5em;
+        }
+
+        /* --- texts --- */
+        a {
+            color: #0099ff;
+            text-decoration: inherit;
+        }
+
+        .mono div {
+            background-color: #e6e7e8;
+            padding: 2.5em;
+            border-radius: 20px;
+        }
+        .mono div p {
+            padding-bottom: 1.3em;
+        }
+        .mono div p:last-child {
+            padding-bottom: 0;
+        }
+    </style>
+
+
+  <!--link rel="stylesheet" href="{{ mix('/css/app.css') }}">
   <link rel="stylesheet"
-      href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/default.min.css">
+      href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/default.min.css"-->
 @endpush
 
 @push('scripts')
-  <script type="text/javascript" src="/js/styleguide.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/highlight.min.js"></script>
+  <!--script type="text/javascript" src="/js/styleguide.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/highlight.min.js"></script-->
 @endpush
